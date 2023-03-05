808159a459b0891fa6069bf71ed2b99a8614e9db
NightRaven <53287573+Xynnix@users.noreply.github.com>
2023-03-04 21:36:14 -0500

    Update ahni.js

--- ahni.js
+++ ahni.js
@@ -1,5 +1,5 @@
 module.exports = {
 	AhniClient: require('./src/Classes/AhniClient'),
-	AhniLists: require('./src/Classes/AhniLists'),
-	AhniCaptcha: require("./src/Classes/AhniCaptcha").AhniCaptcha
+	AhniLists: require('./src/Classes/AhniLists')
+//	AhniCaptcha: require("./src/Classes/AhniCaptcha").AhniCaptcha
 }
