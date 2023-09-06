01f62f673164d10bf775a7cf08b45778858c9810
root <root@vmi1013200.contaboserver.net>
2023-09-06 12:09:58 -0500

    Touchy

--- ahni.js
+++ ahni.js
@@ -1,5 +1,4 @@
 module.exports = {
 	AhniClient: require('./src/Classes/AhniClient'),
 	AhniLists: require('./src/Classes/AhniLists')
-//	AhniCaptcha: require("./src/Classes/AhniCaptcha").AhniCaptcha
 }
